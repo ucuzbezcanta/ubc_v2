c45af5d0e3dde7ff32c1853ed17ed0a90ca60d75
ucuzbezcanta <ucuzbezcanta@gmail.com>
2026-03-02 16:18:08 +0300

    feat: change middleware name again

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,14 +1,14 @@
 import { type NextRequest } from "next/server";
 import { updateSession } from "./utils/supabase/middleware";
 
-export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+// 'default' anahtar kelimesini ekledik ve fonksiyon ismini 'proxy' yaptık
+export default async function proxy(request: NextRequest) {
+  return await updateSession(request);
 }
 
 export const config = {
   matcher: [
-    // /admin ile başlayan tüm yolları bu kontrol mekanizmasına dahil et
     '/admin/:path*',
     '/login',
   ],
-}
+};
