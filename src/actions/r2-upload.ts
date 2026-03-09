ffbffd7149b54b5486354a56e565b79c2f26b7ad
ucuzbezcanta <ucuzbezcanta@gmail.com>
2026-03-09 14:19:33 +0300

    feat:update r2upload

--- src/actions/r2-upload.ts
+++ src/actions/r2-upload.ts
@@ -5,7 +5,7 @@ import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 //R2bağlantı ayarları
 const r2Client = new S3Client({
     region: "auto",
-    endpoint: process.env.R2_ENDPOINT!,
+    endpoint: process.env.R2_ENDPOINT! || `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
     credentials: {
         accessKeyId: process.env.R2_ACCESS_KEY_ID!,
         secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!,
